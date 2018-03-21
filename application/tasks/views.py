c26d5ca5f32814093546d94054391bbcaf2b6323
Lentsku <miika.lampi2@gmail.com>
2018-03-22 01:46:40 +0200

    Nyt localhostissa näkyy tietokanta tasks.db listattuna polussa /tasks. Muutettu myös python-tiedostojen tuplaheittomerkkejä yksittäisiksi heittohipsuiksi

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -2,6 +2,10 @@ from application import app, db
 from flask import render_template, request
 from application.tasks.models import Task
 
+@app.route('/tasks', methods=['GET'])
+def tasks_index():
+    return render_template('tasks/list.html', tasks = Task.query.all())
+
 @app.route('/tasks/new/')
 def tasks_form():
     return render_template('tasks/new.html')
@@ -13,4 +17,4 @@ def tasks_create():
     db.session().add(t)
     db.session().commit()
 
-    return 'hello world!'
+    return redirect(url_for('tasks_index'))
--- application/views.py
+++ application/views.py
@@ -1,6 +1,6 @@
 from application import app
 from flask import render_template
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
