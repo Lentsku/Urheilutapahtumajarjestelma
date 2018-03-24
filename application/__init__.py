e6ea2aa8b8550bd65607a67b89599879de87d0f8
Lentsku <miika.lampi2@gmail.com>
2018-03-24 02:28:15 +0200

    Lisätty graafiset sarakkeet listausnäkymälle. Muutettu myös luotavan tietokannan nimeä ja lisätty *.db .gitignoreen.

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tietokanta.db"
 app.config["SQLALCHEMY_ECHO"] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
