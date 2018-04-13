2c94266edeade06b8413d369698f52ad8928df9b
Lentsku <miika.lampi2@gmail.com>
2018-04-13 23:31:05 +0300

    Typo fix

--- application/models.py
+++ application/models.py
@@ -6,5 +6,5 @@ class Base(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    datemodified = db.Column(db.DateTime, default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp())
